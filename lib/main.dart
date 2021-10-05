933b7086328756dbbec22b2caa01f3e96dadec81
nhatphongcgp <phongnguyen64686969@gmail.com>
2021-10-05 15:51:50 +0700

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
 import './bloc/authentication/authentication_bloc.dart';
 import './bloc/simple_bloc_observer.dart';
 import './repositories/user_repository.dart';
